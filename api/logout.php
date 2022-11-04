a2ce12f719824dd622a8976652df8c474aa18bd0
rex <youngox9@gmail.com>
2022-11-04 09:55:43 +0800

    add

--- api/logout.php
+++ api/logout.php
@@ -1,11 +1,9 @@
 <?php
     require_once dirname(__FILE__).'/facebook_login/initialization.php';
-    session_start();
-    @session_destroy();
+    $domain = $_GET['url'];
     $_SESSION = array(); 
-    $logoutUrl = 'https://www.facebook.com/logout.php?next=http://mydomain.com/logout.php&access_token=' . $helper->getAccessToken();
-
-
+    $logoutUrl = 'https://www.facebook.com/logout.php?next=' . $domain . '&access_token=' . $helper->getAccessToken();
+    @session_destroy();
 
 // if ($_GET['url'] != Null) {
 //     header('location: '.$_GET['url']);
