99692c97713ca2563b4a0a1ce5bf22267b8ed4a2
rex <youngox9@gmail.com>
2022-11-04 10:25:11 +0800

    add

--- api/logout.php
+++ api/logout.php
@@ -1,10 +1,13 @@
 <?php
     require_once dirname(__FILE__).'/facebook_login/initialization.php';
     // require_once dirname(__FILE__).'/facebook_login/initialization.php';
+    if (isset($accessToken)) {
+        require_once dirname(__FILE__) . '/facebook_login/statuslogin.php';
+    }
     if(isset($accessToken)) {
         $url = $helper -> getLogoutUrl($accessToken, 'https://php-demo-mu.vercel.app/api/');
         echo $url;
-    }else {
+     } else {
         $accessToken = $helper-> getAccessToken();
         echo $accessToken;
     }
