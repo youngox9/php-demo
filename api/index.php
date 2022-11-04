1996b442512a8ce5d9f326aef229008340c34207
rex <youngox9@gmail.com>
2022-11-04 10:08:24 +0800

    add

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-    // error_reporting(0);
+    error_reporting(0);
     session_start();
     require_once dirname(__FILE__).'/facebook_login/initialization.php';
     $protocol = 'https://';
