0213e61cdf5b8ac6d6723a1453082ad8f1170024
rex <youngox9@gmail.com>
2022-11-04 10:17:41 +0800

    add

--- api/logout.php
+++ api/logout.php
@@ -1,7 +1,7 @@
 <?php
     require_once dirname(__FILE__).'/facebook_login/initialization.php';
     // require_once dirname(__FILE__).'/facebook_login/initialization.php';
-    $url = $fb -> getLogoutUrl();
+    $url = $helper -> getLogoutUrl();
     echo $url;
 
 ?>
