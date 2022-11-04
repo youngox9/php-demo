ac8a4fa6463e01a0d5720feb5c4587eb44c1ccba
rex <youngox9@gmail.com>
2022-11-04 10:09:23 +0800

    add

--- api/logout.php
+++ api/logout.php
@@ -1,28 +1,7 @@
 <?php
-    require_once dirname(__FILE__).'/facebook_login/initialization.php';
+    // require_once dirname(__FILE__).'/facebook_login/initialization.php';
     $domain = 'https://php-demo-mu.vercel.app/api/';
     session_start(); 
     $_SESSION = []; 
-    
-    // If it's desired to kill the session, also 
-    // delete the session cookie. 
-    // Note: This will destroy the session, and 
-    // not just the session data! 
-    if (ini_get("session.use_cookies")) { 
-        $params = session_get_cookie_params(); 
-        setcookie(session_name(), '', time() - 42000, 
-            $params["path"], $params["domain"], 
-            $params["secure"], $params["httponly"] 
-        ); 
-    } 
-    
-    // Finally, destroy the session. 
     session_destroy();
-
-// if ($_GET['url'] != Null) {
-//     header('location: '.$_GET['url']);
-// } else {
-//     header('location: /');
-// }
-
 ?>
