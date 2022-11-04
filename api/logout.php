49dfb212f707610b61fce8b18a2b287ea2b65b08
rex <youngox9@gmail.com>
2022-11-04 10:07:56 +0800

    add

--- api/logout.php
+++ api/logout.php
@@ -1,9 +1,23 @@
 <?php
     require_once dirname(__FILE__).'/facebook_login/initialization.php';
     $domain = 'https://php-demo-mu.vercel.app/api/';
-    $_SESSION = array(); 
-    $logoutUrl = 'https://www.facebook.com/logout.php?next=' . $domain . '&access_token=' . $accessToken;
-    @session_destroy();
+    session_start(); 
+    $_SESSION = []; 
+    
+    // If it's desired to kill the session, also 
+    // delete the session cookie. 
+    // Note: This will destroy the session, and 
+    // not just the session data! 
+    if (ini_get("session.use_cookies")) { 
+        $params = session_get_cookie_params(); 
+        setcookie(session_name(), '', time() - 42000, 
+            $params["path"], $params["domain"], 
+            $params["secure"], $params["httponly"] 
+        ); 
+    } 
+    
+    // Finally, destroy the session. 
+    session_destroy();
 
 // if ($_GET['url'] != Null) {
 //     header('location: '.$_GET['url']);
@@ -12,6 +26,3 @@
 // }
 
 ?>
-
-<?php echo $helper->getAccessToken() ?>
-<?php echo $logoutUrl; ?>
